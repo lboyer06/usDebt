778a2cc16d4ebad37b93954f66b6abb711ef2397
lboyer06 <lboyer0621@gmail.com>
2024-12-01 01:26:36 -0500

    copy+paste

--- usdebt.py
+++ usdebt.py
@@ -2,11 +2,12 @@
 
 import requests
 from datetime import date, timedelta
-from tkinter import Tk, Button, messagebox
+from tkinter import Tk, Button, messagebox, Label,Text
 
 
 def fetch_total_debt():
 
+    text_box.delete("1.0", "end")
     todaybse =date.today() - timedelta(days=7) #i think the api only shows data from 3 days ago
     today = todaybse.strftime("%Y-%m-%d") #format to match api_url requirements
     #print(today)
@@ -18,28 +19,65 @@ def fetch_total_debt():
             #total_debt = float(data["data"][0]["tot_pub_debt_out_amt"])
             #debt_date = data["data"][0]["record_date"]
             #total_debt = f"{total_debt:,.2f}"
-            
+
             data = response.json()["data"]
             message = ""
-            
-            for record in data:
-                record_date = record["record_date"]
-                total_debt = float(record["tot_pub_debt_out_amt"])
-                message += f"Date : {record_date}, \nTotal Debt: ${total_debt:,.2f}\n\n"
-                
-            messagebox.showinfo("Debt Data", message)
+
+            for i,  record in enumerate(data):
+                if i < len(text_boxes):
+                    record_date = record["record_date"]
+                    total_debt = float(record["tot_pub_debt_out_amt"])
+                    text_boxes[i].delete(1.0,"end")
+                    text_boxes[i].insert("1.0",f"{record_date}")
+                    debt_boxes[i].delete(1.0,"end")
+                    debt_boxes[i].insert("1.0",f"{total_debt:,.2f}")
+
+
+
+
+            #messagebox.showinfo("Debt Data", "Data Pulled from API") #probablyl don't need this
         else:
             messagebox.showerror("error", f"Failed to get data Code: {response.status_code}")
     except Exception as e:
         messagebox.showerror("error!",f"error: , {e}")
-        
- 
+
+
 root = Tk()
 root.title("debt app")
-root.geometry("400x400")
- 
+root.geometry("600x150")
+
+dateLabel = []
+for i in range(3):
+        label1 = Label(text="Date")
+        label1.grid(row=i,column=0)
+        dateLabel.append(label1)
+
+debtLabel = []
+for i in range(3):
+        label1 = Label(text="Debt:")
+        label1.grid(row=i,column=2)
+        debtLabel.append(label1)
+
+
+
+
+text_boxes = []
+for i in range(3):
+        text_box = Text(root, width=12, height=1)
+        text_box.grid(row=i , column=1)
+        text_boxes.append(text_box)
+
 get_data_button = Button(root, text="Get Data", command=fetch_total_debt)
-get_data_button.pack(side="bottom", pady=20)
+get_data_button.grid(column=1,row=3,pady=10)
+
+debt_boxes = []
+for i in range(3):
+        text_box = Text(root, width=12, height=1)
+        text_box.grid(row=i , column=3)
+        debt_boxes.append(text_box)
+
+get_data_button = Button(root, text="Get Data", command=fetch_total_debt)
+get_data_button.grid(column=1,row=3,pady=10)
  
 root.mainloop()
         

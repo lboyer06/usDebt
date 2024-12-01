46eeae0d6504a6086a459c1d21626d0f118bd24e
lboyer06 <lboyer0621@gmail.com>
2024-11-30 23:02:30 -0500

    11/30

--- usdebt.py
+++ usdebt.py
@@ -1,7 +1,8 @@
+
+
 import requests
-import tkinter as tkinter, Button
 from datetime import date, timedelta
-from tkinter import messagebox,simpledialog
+from tkinter import Tk, Button, messagebox
 
 
 def fetch_total_debt():
@@ -9,7 +10,7 @@ def fetch_total_debt():
     todaybse =date.today() - timedelta(days=7) #i think the api only shows data from 3 days ago
     today = todaybse.strftime("%Y-%m-%d") #format to match api_url requirements
     #print(today)
-    api_url = "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/debt_to_penny?filter=record_date:gt:" + today
+    api_url = f"https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/debt_to_penny?filter=record_date:gt:" + today
     try:
         response = requests.get(api_url)
         if response.status_code == 200:
@@ -32,11 +33,19 @@ def fetch_total_debt():
     except Exception as e:
         messagebox.showerror("error!",f"error: , {e}")
         
-        
+ 
+root = Tk()
+root.title("debt app")
+root.geometry("400x400")
+ 
+get_data_button = Button(root, text="Get Data", command=fetch_total_debt)
+get_data_button.pack(side="bottom", pady=20)
+ 
+root.mainloop()
         
  
  
-fetch_total_debt()
+#fetch_total_debt()
 
 
 #print(today)
